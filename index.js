d0cb1c73a37601eb805e3ffbd773ba4276d3ed76
Luiz Duarte <luiz.duarte11@yahoo.com.br>
2024-03-06 16:55:43 -0300

    Add webpage, navigation, and functionality of ip locator. Still need to finish styling and arranging

--- index.js
+++ index.js
@@ -23,32 +23,64 @@
 */
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
+
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// BASE
 
 app.get("/", (req,res) => {
     res.render("index.ejs",{active:"none"});
 });
 
+// IP LOCATOR
+
 app.get("/iplocator",(req,res) => {
-    res.render("index.ejs",{active:"iplocator"});
+    res.render("iplocator.ejs",{active:"iplocator"});
 });
 
+app.post("/iplocator", async (req,res) => {
+    // console.log(req.body);
+
+    try {
+        const response = await axios.get("http://ip-api.com/json/"+req.body.ip);
+
+        // console.log(response);
+        
+        var editedResponse = JSON.stringify(response.data).replaceAll(",","\n");
+        editedResponse = editedResponse.replaceAll("\t","");
+        editedResponse = editedResponse.replaceAll("{","");
+        editedResponse = editedResponse.replaceAll("}","");
+
+        console.log(JSON.stringify(editedResponse));
+
+        res.render("iplocator.ejs",{response:editedResponse});
+    } catch (error) {
+        res.redirect("iplocator.ejs",{response:error});
+    }
+    
+});
+
+// EMAIL VERIFIER
 app.get("/email",(req,res) => {
     res.render("index.ejs",{active:"email"});
 });
 
+// DICTIONARY
 app.get("/dictionary",(req,res) => {
     res.render("index.ejs",{active:"dictionary"});
 });
 
+// QR CODE GENERATOR
 app.get("/qrcode",(req,res) => {
     res.render("index.ejs",{active:"qrcode"});
 });
 
+// JSON BIN
 app.get("/jsonbin",(req,res) => {
     res.render("index.ejs",{active:"jsonbin"});
 });
